0874a66787ef427b94c7498c167a552ddda25959
tnshine <tnshine@gmail.com>
2016-09-02 01:59:13 +0000

    add initial setups

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module Microposts
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Lesson7 chapter-2.2
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+
   end
 end
