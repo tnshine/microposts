e040e546d3eb2b9776826b5b97a4bd9b23c1becf
tnshine <tnshine@gmail.com>
2016-09-02 15:07:05 +0000

    add column description/location

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update]
+  before_action :authority_check, only: [:edit, :update]
 
   def show
-   @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create
@@ -17,11 +23,32 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "updated user data"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :description, :location)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # 対象レコードの編集権限をチェック
+  def authority_check
+    # ログイン中のユーザーIDと、対象データのユーザーIDが一致しない場合はリダイレクト
+    unless session[:user_id] == @user.id
+      redirect_to @user
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
     has_secure_password
+    validates :description, length: { maximum: 255 }
+    validates :location, length: { maximum: 255 }
 end
